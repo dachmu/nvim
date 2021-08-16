1ed81a05a5daa44ff1f6b97a9e735bafcc7e1915
David Chaverra <david.chaverra.dev@gmail.com>
2021-08-16 03:51:43 -0500

    Added a few good pluggins

--- init.lua
+++ init.lua
@@ -12,7 +12,24 @@ require('packer').startup(function()
     -- Nice color scheme
     use 'sts10/vim-pink-moon'
 
-    
+    -- LSP confs, remember to set up the servers
+    use 'neovim/nvim-lspconfig'
+
+    -- Prettier LSP uis
+    use 'glepnir/lspsaga.nvim'
+
+    -- Pretty Icons
+    use 'kyazdani42/nvim-web-devicons'
+
+    -- Better Syntax hightlights, use :TSInstall <language> to install a language 
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+    -- Better fuzzy finding and preview
+    use 'nvim-lua/popup.nvim'
+    use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
 
     -- add other use ... for other packer
 end)
+
+require("treesitter_config")
+require("telescope_config")
