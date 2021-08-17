7960614633d4d73dd1d47ec9aa138301c39f6e3e
David Chaverra <david.chaverra.dev@gmail.com>
2021-08-16 21:45:58 -0500

    Adding some native lsp

---
+++ dependencies.sh
@@ -0,0 +1,8 @@
+#
+
+# Simple dependencies installer
+yarn global add dockerfile-language-server-nodejs
+yarn global add bash-languag-servere
+yarn global add yaml-language-server
+
+# Remember to install ninja: pacman -S ninja
--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require('settings')
 require('keymaps')
 require('colorscheme')
 
-require('packer').startup(function()
+require('packer').startup(function(use)
     -- Packer manages itself
     use 'wbthomason/packer.nvim'
 
@@ -21,15 +21,20 @@ require('packer').startup(function()
     -- Pretty Icons
     use 'kyazdani42/nvim-web-devicons'
 
-    -- Better Syntax hightlights, use :TSInstall <language> to install a language 
+    -- Better Syntax hightlights, use :TSInstall <language> to install a language
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
     -- Better fuzzy finding and preview
     use 'nvim-lua/popup.nvim'
     use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
 
+    -- Better signatures
+    use 'ray-x/lsp_signature.nvim'
+
     -- add other use ... for other packer
 end)
 
 require("treesitter_config")
 require("telescope_config")
+require("lsp_signature_config")
+require("lsp_language_servers_config")
