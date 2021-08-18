787dafdc9f6f8f83e2737b71920e7407df908737
David Chaverra <david.chaverra.dev@gmail.com>
2021-08-18 18:14:32 -0500

    Adding some facy keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,6 +2,30 @@
 
 vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap( "n" ,"<F6>", ":setlocal spell! spelllang=en_us<CR>", {noremap = true})
-vim.api.nvim_set_keymap( "n" ,"<F7>", ":setlocal spell! spelllang=es<CR>", {noremap = true})
-vim.api.nvim_set_keymap( "n" ,"<F11>", ":setlocal spell!", {noremap = true})
+-- Sets the dictionary for my working laguage
+vim.api.nvim_set_keymap( "n", "<F6>", ":setlocal spell! spelllang=en_us<CR>", {noremap = true})
+vim.api.nvim_set_keymap( "n", "<F7>", ":setlocal spell! spelllang=es<CR>", {noremap = true})
+vim.api.nvim_set_keymap( "n", "<F11>", ":setlocal spell!", {noremap = true})
+
+-- Makes the Y behave like most capital letters
+vim.api.nvim_set_keymap( "n", "Y", "y$", {noremap = true})
+
+-- Keeps the cursor centered while performing the operations
+vim.api.nvim_set_keymap( "n", "n", "nzzzv", {noremap = true})
+vim.api.nvim_set_keymap( "n", "N", "Nzzzv", {noremap = true})
+vim.api.nvim_set_keymap( "n", "J", "mzJ'z", {noremap = true})
+
+-- Makes certain characters add more breakpoints, for undo, in insert mode
+vim.api.nvim_set_keymap( "i", ",", ",<c-g>u", {noremap = true})
+vim.api.nvim_set_keymap( "i", ".", ".<c-g>u", {noremap = true})
+vim.api.nvim_set_keymap( "i", ":", ":<c-g>u", {noremap = true})
+vim.api.nvim_set_keymap( "i", ";", ";<c-g>u", {noremap = true})
+
+-- Adding some Fancy moving
+vim.api.nvim_set_keymap( "v", "J", ":m '>+1<CR>gv=gv", {noremap = true})
+vim.api.nvim_set_keymap( "v", "K", ":m '<-2<CR>gv=gv", {noremap = true})
+vim.api.nvim_set_keymap( "i", "<C-j>", "<esc>:m .+1<CR>==", {noremap = true})
+vim.api.nvim_set_keymap( "i", "<C-j>", "<esc>:m .+1<CR>==", {noremap = true})
+vim.api.nvim_set_keymap( "n", "<leader>j", ":m .+1<CR>==", {noremap = true})
+vim.api.nvim_set_keymap( "n", "<leader>k", ":m .-2<CR>==", {noremap = true})
+
--- lua/settings.lua
+++ lua/settings.lua
@@ -14,7 +14,7 @@ vim.opt.shiftwidth = tab_size
 vim.bo.fileformat = "unix" -- Makes sure to use unix format files
 vim.o.ignorecase = true -- Makes default searches case insesitive
 vim.o.smartcase = true -- When searching, vim will be case insensitive if the search start in lowercase, and case sensitive otherwise.
-vim.o.scrolloff = 4  -- Keeps the cursor centered when moving throught the document
+vim.o.scrolloff = 8  -- Keeps the cursor centered when moving throught the document
 
 vim.o.splitbelow = true  -- Makes default vertical split to go down
 vim.o.splitright = true  -- Makes default side split window to the right
