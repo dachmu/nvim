1f8b58130dcddb45e68128f4bfff27a2c3b9ef70
Daluxm <david.chaverra.dev@gmail.com>
2022-05-17 07:14:33 -0500

    Changing Colorscheme

--- lua/colorscheme.lua
+++
@@ -1,8 +0,0 @@
--- Configure here the color scheme of the editor
-
-vim.o.termguicolors = true -- Makes terminal look pretty, if suported
-if pcall(function() vim.cmd "colorscheme pink-moon" end) then
-    -- ColorScheme loaded
-else
-    vim.cmd "colorscheme desert"
-end
--- lua/settings/colorscheme.lua
+++ lua/settings/colorscheme.lua
@@ -2,7 +2,21 @@
 
 vim.o.termguicolors = true -- Makes terminal look pretty, if suported
 
-local color_scheme = "nord"
+-- Colorschemes:
+--    zenwritten	    Zero hue and saturation version
+--    neobones	        Inspired by neovim.io
+--    vimbones	        Inspired by vim.org
+--    rosebones	        Inspired by Rosé Pine
+--    forestbones	    Inspired by Everforest
+--    nordbones	        Inspired by Nord
+--    tokyobones	    Inspired by Tokyo Night
+--    seoulbones	    Inspired by Seoul256
+--    duckbones	        Inspired by Spaceduck
+--    zenburned	        Inspired by Zenburn
+--    kanagawabones	    Inspired by Kanagawa
+--    randombones	    Randomly pick from the collection.
+
+local color_scheme = "rosebones"
 
 local set_scheme = string.format("colorscheme %s", color_scheme)
 local set_default_scheme = "colorscheme desert"
